116d1c59eec73146f2e7b72975b489fd8aabc869
Uladzimir Huryeu <uladzimir.g@gmail.com>
2022-03-10 19:47:41 +0000

    send data to file

--- Uladzimir_Huryeu/06.bash/10_biggest_file_in_dir.sh
+++ Uladzimir_Huryeu/06.bash/10_biggest_file_in_dir.sh
@@ -3,3 +3,5 @@ echo "Enter the absolute path to directory"
 read P
 cd $P
 ls -alhS | grep '^-' | head -10
+ls -alhS | grep '^-' | head -10 > /home/student/belhard_class/06.bash/10_biggest_files.txt
+echo "To see result view 10_biggest_files.txt"
