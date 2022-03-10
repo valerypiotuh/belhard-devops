3df1ab4f0b88fcf89fa35e107d4927caf65cadc6
Uladzimir Huryeu <uladzimir.g@gmail.com>
2022-03-10 20:49:30 +0000

    some changes

--- Uladzimir_Huryeu/06.bash/10_biggest_file_in_dir.sh
+++ Uladzimir_Huryeu/06.bash/10_biggest_file_in_dir.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+echo
 echo "Enter the absolute path to directory"
 read P
+echo
 cd $P
+exec 6>&1
+exec > /home/student/belhard_class/06.bash/10_biggest_files.txt
+echo "10 biggest files in directory"
+pwd
 ls -alhS | grep '^-' | head -10
-ls -alhS | grep '^-' | head -10 > /home/student/belhard_class/06.bash/10_biggest_files.txt
-echo "To see result view 10_biggest_files.txt"
+echo
+echo "All resuls saved to 10_biggest_files.txt"
+echo
+exec 1>&6 6>&-
+cat /home/student/belhard_class/06.bash/10_biggest_files.txt
