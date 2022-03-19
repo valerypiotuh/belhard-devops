b3c0f81d44490a78fbdbdee1689239650735264b
a-voloshchik <100310248+a-voloshchik@users.noreply.github.com>
2022-03-19 20:27:42 +0300

    Delete Dmitry_Potapov directory

--- Dmitry_Potapov/06.BASH/scriptcountwords.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Текстовых файлов со словом fuck: `grep -Rwi "fuck*" /home/dima/linux/linux-5.16.13 | wc -l`" > badwords.txt
-echo "Текстовых файлов со словом shit: `grep -Rwi "shit*" /home/dima/linux/linux-5.16.13 | wc -l`" >> badwords.txt
-echo "Текстовых файлов со словом crap: `grep -Rwi "crap*" /home/dima/linux/linux-5.16.13 | wc -l`" >> badwords.txt
-echo "Текстовых файлов со словом bastard: `grep -Rwi "bastard" /home/dima/linux/linux-5.16.13 | wc -l`" >> badwords.txt
-echo "Текстовых файлов со словом penguin: `grep -Rwi "penguin" /home/dima/linux/linux-5.16.13 | wc -l`" >> badwords.txt
