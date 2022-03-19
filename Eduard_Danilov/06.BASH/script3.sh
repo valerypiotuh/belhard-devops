0ad32f5a541f1eb7aff6715c635882bdd8a8b710
a-voloshchik <100310248+a-voloshchik@users.noreply.github.com>
2022-03-19 20:27:52 +0300

    Delete Eduard_Danilov directory

--- Eduard_Danilov/06.BASH/script3.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-read -e -p "Введи каталог, в котором следует искать большие файлы: " filepath
-echo $filepath
-echo "Введи имя файла, в который нужно сохранить результат"
-read otchet
-du -ahx $filepath | sort -rh | head -10 > $otchet
-echo "Операция выполнена. Результат в файле $otchet"
