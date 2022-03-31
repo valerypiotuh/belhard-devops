a990cf8ffb16d179057e3ed107167ee5c3412029
Viktor_Brilevich <v.brilik29@gmail.com>
2022-03-31 15:20:19 +0000

    12Jenkins push Jenkinsfile part 2

--- Viktor_Brilevich/12.Jenkins/part2/entrypoint.sh
+++ Viktor_Brilevich/12.Jenkins/part2/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apk update && apk add --no-cach curl wget git bash speedtest-cli
-date > speedtest.log && speedtest-cli >> speedtest.log
+date > /app/speedtest.log && speedtest-cli >> /app/speedtest.log
