a8a78a5876f4725bacd9d60b101ab51f8da95a96
Viktor_Brilevich <v.brilik29@gmail.com>
2022-04-11 16:51:04 +0000

    XX class error

--- Viktor_Brilevich/XX.Final_Project/demo/start_web.sh
+++ Viktor_Brilevich/XX.Final_Project/demo/start_web.sh
@@ -1,2 +1,2 @@
-ansible-playbook -i ~/demo/hosts ~/demo/playbooks/playbook_october.yml
-ansible-playbook -i ~/demo/hosts ~/demo/playbooks/playbook_backdrop.yml
+ansible-playbooks -i ~/demo/hosts ~/demo/playbooks/playbook_october.yml
+ansible-playbooks -i ~/demo/hosts ~/demo/playbooks/playbook_backdrop.yml
