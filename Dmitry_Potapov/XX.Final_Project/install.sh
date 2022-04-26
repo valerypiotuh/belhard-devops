7c7e26b0b47a95c9c77d5e1ee275056915fbd3b2
Dmitry Potapov <magaton2011@gmail.com>
2022-04-26 21:54:24 +0300

    add templates of Final Project

---
+++ Dmitry_Potapov/XX.Final_Project/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "hello, just install some instruments"
+#git init
+#git clone git@github.com:megaton20111/belhard-devops-1.git
+echo "git repo was cloned in locally folder"
+
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+#sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+#sudo apt-get update && sudo apt-get install terraform
+#mkdir Terraform
+#cd Terraform
+#wget https://releases.hashicorp.com/terraform/1.1.9/terraform_1.1.9_linux_amd64.zip 
+#unzip terraform_1.1.9_linux_amd64.zip
+#rm terraform_1.1.9_linux_amd64.zip
+#sudo mv terraform /bin/
+#terraform --version
+echo "terraform was installed"
+
+
