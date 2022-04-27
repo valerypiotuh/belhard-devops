de908cb46d597b9b45d0883e53d93e099deaab65
Dmitry Potapov <megaton2011@gmail.com>
2022-04-27 18:43:10 +0300

    !

--- Dmitry_Potapov/XX.Final_Project/install.sh
+++ Dmitry_Potapov/XX.Final_Project/install.sh
@@ -14,11 +14,24 @@ echo "git repo was cloned in locally folder"
 #mkdir Terraform
 #cd Terraform
 #RiseupVPN &
-#wget https://releases.hashicorp.com/terraform/1.1.9/terraform_1.1.9_linux_amd64.zip 
+#wget https://releases.hashicorp.com/terraform/1.1.9/terraform_1.1.9_linux_amd64.zip
 #unzip terraform_1.1.9_linux_amd64.zip
 #rm terraform_1.1.9_linux_amd64.zip
 #sudo mv terraform /bin/
 #terraform --version
 echo "terraform was installed"
-
-
+echo "inslall atom editor on lokal machine"
+#add our official package repository to your system by running the following commands:
+#wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+#sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+#install Atom using apt-get (or apt on Ubuntu):
+#sudo apt-get update
+# Install Atom
+#sudo apt-get install atom
+# Install Atom Beta
+#sudo apt-get install atom-beta
+#download the Atom .deb package and install it directly:
+# Install Atom
+#sudo dpkg -i atom-amd64.deb
+# Install Atom's dependencies if they are missing
+#sudo apt-get -f install
