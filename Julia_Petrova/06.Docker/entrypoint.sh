fcee634b1552024881d446444e7f383cf92b8f46
juliaviolet666 <102578912+juliaviolet666@users.noreply.github.com>
2022-06-16 19:18:24 +0300

    Update entrypoint.sh

--- Julia_Petrova/06.Docker/entrypoint.sh
+++ Julia_Petrova/06.Docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir /app
-apk update && apk add no-cache curl wget git bash speedtest-cli
+apk update && apk add --no-cache curl wget git bash speedtest-cli
 speedtest-cli --bytes >> log.txt 
