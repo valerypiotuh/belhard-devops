0f2027dbcfedcb4ea80df3283a3ec1056ce4c7ce
pivanaivy <asoi720603@gmail.com>
2022-06-21 08:10:52 +0000

    RM Files from 06.docker

--- Alex_Khomets/06.docker/entrypoint_mount.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-apk update && apk add curl wget git bash 
-wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
-chmod +x speedtest-cli
-apk update && apk add speedtest-cli
-speedtest-cli --bytes > log.txt
