ceb40ab584aa5a3b3cf5fba221529755ee3861fc
sergkharkin@gmail.com <sergkharin@gmail.com>
2022-06-28 00:15:30 +0300

    Добавил домашку по Terraform

---
+++ Sergey_Kharkin/10.Terraform/DOCKER/variables.tf
@@ -0,0 +1,14 @@
+variable "pgadmin_port" {
+  type    = string
+  default = "8050"
+}
+
+variable "nginx_port" {
+  type    = string
+  default = "8801"
+}
+
+variable "postgres_port" {
+  type    = string
+  default = "8032"
+}
