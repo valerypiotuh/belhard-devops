ae5b6f212c12a5ff7a7651786e74e01975e4986a
Alexey Golovnev <golovnev.work@gmail.com>
2022-07-12 00:15:13 +0000

    09.Terraform - Alexey Golovnev

---
+++ Alexey_Golovnev/09.TERRAFORM/virtualbox/variables.tf
@@ -0,0 +1,14 @@
+variable "nodes_count" {
+  type    = string
+  default = "1"
+}
+
+variable "cpus" {
+  type    = string
+  default = "1"
+}
+
+variable "memory" {
+  type    = string
+  default = "1024 mib"
+}
