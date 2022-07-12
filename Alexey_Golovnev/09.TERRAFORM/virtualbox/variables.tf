c5420b6c174e214feca8e52395396a4c5f77592c
Alexey Golovnev <golovnev.work@gmail.com>
2022-07-12 00:24:43 +0000

    09.Terraform - Alexey Golovnev: added description of variables

--- Alexey_Golovnev/09.TERRAFORM/virtualbox/variables.tf
+++ Alexey_Golovnev/09.TERRAFORM/virtualbox/variables.tf
@@ -1,14 +1,17 @@
 variable "nodes_count" {
-  type    = string
-  default = "1"
+  description = "The number of required nodes."
+  type        = string
+  default     = "1"
 }
 
 variable "cpus" {
-  type    = string
-  default = "1"
+  description = "The number of allocated CPU cores for one node."
+  type        = string
+  default     = "1"
 }
 
 variable "memory" {
-  type    = string
-  default = "1024 mib"
+  description = "The amount of allocated RAM for one node."
+  type        = string
+  default     = "1024 mib"
 }
