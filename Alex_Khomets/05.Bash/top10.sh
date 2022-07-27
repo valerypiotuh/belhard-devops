d1b53572d3ca6e786f64059ce5fc203cd4722299
pivanaivy <asoi720603@gmail.com>
2022-07-27 16:53:35 +0000

    test

--- Alex_Khomets/05.Bash/top10.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-find ${testpath} -type f -exec ls -s {} \; | sort -n -r | head -10 >> top10.txt
-echo "" >> top10.txt
--- Alex_Khomets/05.Bash/word_col.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-col=$(grep -ioar $1 $2 | wc -l)
-echo "'$1' count: " $col >> word_col.txt
